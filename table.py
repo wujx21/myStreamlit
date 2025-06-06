0d0490b64d2a9dbff5868343063043fe185a8d22
xianjing <304218002@qq.com>
2025-06-06 09:43:59 +0800

    Add files via upload

---
+++ table.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+
+# import pandas as pd
+
+df = pd.DataFrame(
+    {
+        "first column": [0, 1, 2, 3, 4],
+        "second column": [0, 10, 20, 30, 40],
+    }
+)
+#df
+
+st.write("Below is a DataFrame:", df, "Above is a dataframe.")
+
