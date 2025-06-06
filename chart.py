d9e83430bf04c083e1961ec685ec636604b331ef
xianjing <304218002@qq.com>
2025-06-06 09:26:19 +0800

    Add files via upload

---
+++ chart.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import altair as alt
+import streamlit as st
+
+df2 = pd.DataFrame(np.random.randn(200, 3), columns=["a", "b", "c"])
+c = (
+    alt.Chart(df2)
+    .mark_circle()
+    .encode(x="a", y="b", size="c", color="c", tooltip=["a", "b", "c"])
+)
+st.write(c)
